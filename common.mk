ac9b32d087fe94b20d8783a32e468cab6899d03d
verNANDo57 <silvenation@gmail.com>
2020-11-02 00:56:56 +0700

    sm6250-common: fix dependencies

--- common.mk
+++ common.mk
@@ -381,7 +381,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service.xiaomi_sm6250
+    android.hardware.power@1.2-service.redmi_sm6250
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \
