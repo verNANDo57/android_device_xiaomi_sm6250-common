df8d8262bbede788419a896175fadbbef9a0e94c
verNANDo57 <silvenation@gmail.com>
2020-11-04 00:09:44 +0700

    sm6250-common: import some camera libs

--- common.mk
+++ common.mk
@@ -193,6 +193,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libshim_megvii
 
+PRODUCT_PACKAGES += \
+    android.hardware.camera.device@3.4 \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64 \
+    android.hardware.camera.provider@2.5 \
+    libdng_sdk.vendor \
+    vendor.qti.hardware.camera.device@1.0
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
