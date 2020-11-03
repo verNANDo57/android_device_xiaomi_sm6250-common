70adaa21485a6c46745835fa787fc15476a98067
verNANDo57 <silvenation@gmail.com>
2020-11-03 13:12:05 +0700

    sm6250-common: import vendor.qti.hardware.btconfigstore@1.0.vendor

--- common.mk
+++ common.mk
@@ -131,7 +131,8 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    BluetoothQti
+    BluetoothQti \
+    vendor.qti.hardware.btconfigstore@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
