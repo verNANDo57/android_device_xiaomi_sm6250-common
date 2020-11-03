ca3cb9697be22356756e1fff6e3b065ac7d78ee2
verNANDo57 <silvenation@gmail.com>
2020-11-03 13:35:55 +0700

    sm6250-common: setup display stuff

--- common.mk
+++ common.mk
@@ -413,6 +413,39 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
+# Display
+PRODUCT_PACKAGES += \
+    gralloc.atoll \
+    hwcomposer.atoll \
+    memtrack.atoll \
+    libdisplayconfig \
+    libdisplayconfig.vendor \
+    liboverlay \
+    libtinyxml \
+    libvulkan \
+    libqdMetaData \
+    libqdMetaData.system
+
+PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0_32 \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.mapper@2.0-impl-qti-display \
+    android.hardware.graphics.composer@2.3-service \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
+    android.hardware.renderscript@1.0-impl \
+    android.frameworks.displayservice@1.0 \
+    vendor.qti.hardware.display.allocator-service \
+    vendor.display.config@1.1 \
+    vendor.display.config@1.1_vendor
+
+# Display interfaces
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.display.composer@1.0.vendor \
+    vendor.qti.hardware.display.composer@2.0.vendor \
+    vendor.qti.hardware.display.mapper@1.1.vendor
+
 # TextClassifier
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
