14171eb1fe6347b2c4b16220a384239b08bd3855
verNANDo57 <silvenation@gmail.com>
2020-11-03 13:09:05 +0700

    sm6250-common: import atoll thermal stuff

--- common.mk
+++ common.mk
@@ -99,6 +99,11 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    thermal.atoll
 # IRQ
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
