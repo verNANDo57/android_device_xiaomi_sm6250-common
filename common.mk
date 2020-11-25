39ebb6095b2add37ada1e46b2ddf9cf6d407738c
verNANDo57 <silvenation@gmail.com>
2020-11-25 19:37:08 +0700

    sm6250-common: set build super partitions to false

--- common.mk
+++ common.mk
@@ -354,6 +354,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
 # Partitions
+PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
