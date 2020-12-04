6aabcd780a0c87883cc31e0b44789ecb096b1495
verNANDo57 <silvenation@gmail.com>
2020-12-04 18:40:56 +0700

    sm6250-common: update bt config

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -22,8 +22,8 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-#define BLUETOOTH_QTI_SW TRUE
 #define BTM_DEF_LOCAL_NAME "Redmi Note 9 Series"
+#define BLUETOOTH_QTI_SW TRUE
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    32
