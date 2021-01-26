479cf55f419de30a2a4638461456d4184f08280a
verNANDo57 <silvenation@gmail.com>
2021-01-26 18:03:55 +0700

    sm6250-common: move init-on-boot script to android_device_xiaomi_joyeuse repo

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -141,10 +141,6 @@ DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/configs/manifests/framework_man
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/configs/manifests/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/configs/manifests/compatibility_matrix.xml
 
-# Init
-TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):libinit_xiaomi_sm6250
-TARGET_RECOVERY_DEVICE_MODULES := libinit_xiaomi_sm6250
-
 # Keystore
 TARGET_PROVIDES_KEYMASTER := true
 
